e57db52495aae93315c65782aa393e0147f5c366
fmvilas <fmvilas@gmail.com>
2019-04-03 13:39:14 +0200

    Add binaries for multiple platforms, clean API and tests, and various code organization improvements.

--- test/parseTest.js
+++ test/parseTest.js
@@ -1,21 +1,22 @@
-// var assert = require('assert');
-var expect = require('chai').expect;
-var parser = require('../parser');
+const expect = require('chai').expect;
+const parser = require('../lib/parser');
 const fs = require('fs');
 const path = require("path");
 
-describe('parser()', function () {
-    it('should parse yaml', function () {
-        const yamlDoc = fs.readFileSync(path.resolve(__dirname, "./async.yaml"), 'utf8');
-        var result = parser(yamlDoc);
-        expect(result.hasErrors).to.equal(false);
-    });
+const asyncapiYAML = '{"x-parser-messages":[{"message":{"payload":{"properties":{"name":{"type":"strin"}},"type":"object"}},"channelName":"mychannel","operationName":"publish"}],"asyncapi":"2.0.0","channels":{"mychannel":{"publish":{"message":{"payload":{"properties":{"name":{"type":"strin"}},"type":"object"}}}}},"id":"myapi","info":{"title":"My API","version":"1.0.0"}}';
+
+describe('parse()', function () {
+  it('should parse yaml', function () {
+    const yamlDoc = fs.readFileSync(path.resolve(__dirname, "./asyncapi.yaml"), 'utf8');
+    const result = parser(yamlDoc);
+    expect(JSON.stringify(result)).to.equal(asyncapiYAML);
   });
+});
 
 //   describe('parser()', function () {
 //     it('should parse json', function () {
-//         const jsonDoc = fs.readFileSync(path.resolve(__dirname, "./async.json"), 'utf8');
-//         var result = parser(jsonDoc);
+//         const jsonDoc = fs.readFileSync(path.resolve(__dirname, "./asyncapi.json"), 'utf8');
+//         const result = parser(jsonDoc);
 //         expect(result.hasErrors).to.equal(false);
 //     });
 //   });

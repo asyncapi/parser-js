d2cd2086d4c7fcd46290cccc2cd52a7c33240840
derberg <lpgornicki@gmail.com>
2020-07-16 17:58:58 +0200

    final?

--- README.md
+++ README.md
@@ -134,8 +134,7 @@ For more information about the `ParserError` class, [check out the documentation
 
 Parser dereferences all circular references by default. In addition, to simplify interactions with the parser, the following is added:
 - `x-parser-circular` property is added to the root of AsyncAPI document to indicate that document contains circular references. Tooling developer that doesn't want to support circular references and quickly fail any action by checking this field or a special method `hasCircular`
-- `x-parser-circular` property is added to every schema that contains circular references, you can also use `isCircular` method on a Schema model like `document.components().schema('mySchema').isCircular()`
-- `x-parser-circular` property is added to every object where circular reference starts.
+- `x-parser-circular` property is added to every schema where circular reference starts. You should use `isCircular` method on a Schema model like `document.components().schema('RecursiveSelf').properties()['selfChildren'].isCircular()`
 
 ### Develop
 
--- test/parse_test.js
+++ test/parse_test.js
@@ -20,7 +20,6 @@ const invalidYamlOutput = '{"asyncapi":"2.0.0","info":{"version":"1.0.0"},"chann
 const invalidJsonOutput = '{"asyncapi":"2.0.0","info":{"version":"1.0.0"},"channels":{"mychannel":{"publish":{"message":{"payload":{"type":"object","properties":{"name":{"type":"string"}}}}}}},"components":{"messages":{"testMessage":{"payload":{"type":"object","properties":{"name":{"type":"string"},"test":{"type":"object","properties":{"testing":{"type":"string"}}}}}}},"schemas":{"testSchema":{"type":"object","properties":{"name":{"type":"string"},"test":{"type":"object","properties":{"testing":{"type":"string"}}}}}}}}';
 const outputJsonWithRefs = '{"asyncapi":"2.0.0","info":{"title":"My API","version":"1.0.0"},"channels":{"mychannel":{"publish":{"traits":[{"$ref":"#/components/operationTraits/docs"}],"externalDocs":{"x-extension":true,"url":"https://irrelevant.com"},"message":{"$ref":"#/components/messages/testMessage"}}},"oneOfMessageChannel":{"publish":{"message":{"oneOf":[{"$ref":"#/components/messages/testMessage"}]}}}},"components":{"messages":{"testMessage":{"traits":[{"$ref":"#/components/messageTraits/extension"}],"payload":{"$ref":"#/components/schemas/testSchema"}}},"schemas":{"testSchema":{"type":"object","properties":{"name":{"type":"string"},"test":{"$ref":"refs/refed.yaml"}}}},"messageTraits":{"extension":{"x-some-extension":"some extension"}},"operationTraits":{"docs":{"externalDocs":{"url":"https://company.com/docs"}}}}}';
 const invalidAsyncAPI = { asyncapi: '2.0.0', info: {} };
-const xParserCircle = 'x-parser-circular';
 
 const eolLength = EOL.length;
 
@@ -382,15 +381,16 @@ describe('parse()', function() {
 
   it('should properly mark circular references', async function() {
     const result = await parser.parse(inputYAMLCircular, { path: __filename });
-    expect(result.components().schema('RecursiveAncestor').properties().children._json.items[xParserCircle]).to.equal(true);
-    expect(result.components().schema('RecursiveSelf').properties().something._json.properties.test.properties.children.items[xParserCircle]).to.equal(true);
-    expect(result.channel('external/file').publish().json().message.payload.properties.test.properties.children.items[xParserCircle]).to.equal(true);
-    expect(result.json()[xParserCircle]).to.equal(true);
+    expect(result.components().schema('RecursiveAncestor').properties()['ancestorChildren'].isCircular()).to.equal(true);
+    expect(result.components().schema('RecursiveSelf').properties()['selfSomething'].properties()['test'].properties()['ancestorChildren'].isCircular()).to.equal(true);
+    expect(result.channel('external/file').publish().messages()[0].payload().properties()['testExt'].properties()['children'].isCircular()).to.equal(true);
     //not testing on a model level as required xParserCircle value is added before model construction so we need to test through calling parser function
     expect(result.hasCircular()).to.equal(true);
-    expect(result.components().schema('RecursiveSelf').isCircular()).to.equal(true);
+    //we want false here, even though this schema has some circular refs in some props, it is not circular, but just specific items
+    expect(result.components().schema('RecursiveSelf').isCircular()).to.equal(false);
     expect(result.components().schema('NonRecursive').isCircular()).to.equal(false);
-    expect(result.components().schema('NonRecursive').properties().child[xParserCircle]).to.equal(undefined);
+    expect(result.components().schema('RecursiveSelf').properties()['selfChildren'].isCircular()).to.equal(true);
+    expect(result.components().schema('NonRecursive').properties()['child'].isCircular()).to.equal(false);
   });
 });
 

53589c21eae27563ac6e2b55f61ee33e5ffc9f1a
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2020-09-10 13:11:52 +0200

    Make script to remove broken types in types.d.ts

---
+++ scripts/fix-ts-types.js
@@ -0,0 +1,14 @@
+/**
+ * This script removes unnecessary and broken types from "types.d.ts" file
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+const tsFile = path.resolve(__dirname, "../types.d.ts");
+
+const data = fs.readFileSync(tsFile, 'utf-8');
+const namespaceRegex = /(declare namespace)[ A-Za-z]*{((.|\n)+?)(?!(};))}/g;
+const newData = data.replace(namespaceRegex, "");
+
+fs.writeFileSync(tsFile, newData, 'utf-8');

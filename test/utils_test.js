ad2f6f0f0a8607a4a4a3e687061138cc76b88f78
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2020-09-13 12:51:39 +0200

    Fix linter

--- test/utils_test.js
+++ test/utils_test.js
@@ -5,20 +5,22 @@ const utils = require('../lib/utils');
 describe('utils', function() {
   describe('mix()', function() {
     const Mixin = {
-      utilFn: function() {},
+      utilFn() {},
     };
-    class Base {}
-    const Model = utils.mix(Base, Mixin);
 
     it('should create mixed object', function() {
+      const Model = utils.mix(class {}, Mixin);
+
       expect(Model.prototype.utilFn).not.to.be.equal(undefined);
       expect(Model.prototype.utilFn === Mixin.utilFn).to.be.equal(true);
     });
     
     it('should throw error if one of mixes is model reference', function() {
+      class Base {}
+
       try {
         utils.mix(Base, Mixin, Base);
-      } catch(e) {
+      } catch (e) {
         expect(e).not.to.be.equal(undefined);
         expect(e.message).to.be.equal(`invalid mix function: cannot use the model ${Base} as a mixin`);
       }

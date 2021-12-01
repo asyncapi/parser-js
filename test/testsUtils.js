777dccbf66863e233b4a165f6efd09dd449f3c44
Matatjahu <urbanczyk.maciej.95@gmail.com>
2021-12-01 13:50:48 +0100

    fix: offset in windows

--- test/testsUtils.js
+++ test/testsUtils.js
@@ -1,5 +1,7 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const { EOL } = require('os');
+const eolLength = EOL.length;
 const ParserError = require('../lib/errors/parser-error');
 
 chai.use(chaiAsPromised);
@@ -12,10 +14,11 @@ const testsUtils = module.exports;
  * 
  * @function offset
  * @private
- * @param  {Number} oset end or start offset number
+ * @param {Number} oset end or start offset number
+ * @param {Number} line end or start line number
  * @returns {Number} calculated offset number
  */
-testsUtils.offset = (oset) => oset;
+testsUtils.offset = (oset, line) => (oset + ((eolLength - 1) * (line - 1)));
 
 /* eslint-disable sonarjs/cognitive-complexity */
 /**

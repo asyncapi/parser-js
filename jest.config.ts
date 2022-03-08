5be4fb5edea4f8508e2c0adefcaf91b42170a50f
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2022-03-08 15:29:07 +0100

    chore: init architecture (#484)

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  coverageReporters: [
+    'json-summary',
+    'lcov',
+    'text'
+  ],
+  preset: 'ts-jest',
+  // The root of your source code, typically /src
+  // `<rootDir>` is a token Jest substitutes
+  roots: ['<rootDir>'],
+  
+  // Test spec file resolution pattern
+  // Matches parent folder `__tests__` and filename
+  // should contain `test` or `spec`.
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  // Module file extensions for importing
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testTimeout: 10000,
+  collectCoverageFrom: [
+    'src/**'
+  ],
+};
+
+export default config;
---
+++ src/models/index.ts
@@ -0,0 +1,2 @@
+export * from './asyncapi';
+export * from './base';

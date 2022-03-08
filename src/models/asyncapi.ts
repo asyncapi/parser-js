868755a654271d183ee86f7904e66e204a3cdbe6
Sergio Moya <1083296+smoya@users.noreply.github.com>
2022-03-08 17:39:17 +0100

    feat: add info and related models

--- src/models/asyncapi.ts
+++ src/models/asyncapi.ts
@@ -1,3 +1,13 @@
 import { BaseModel } from "./base";
+import { Info } from "./info";
 
-export class AsyncAPIDocument extends BaseModel {}
+export class AsyncAPIDocument extends BaseModel {
+    version(): string {
+        return this.json("asyncapi");
+    }
+
+    info(): Info {
+        const doc = this.json("info");
+        return doc && new Info(doc);
+    }
+}

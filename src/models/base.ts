81cc4eca42bb1b1df8c2bb94a182ce803efed21f
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2022-03-08 15:29:07 +0100

    chore: init architecture (#484)

---
+++ src/models/base.ts
@@ -0,0 +1,11 @@
+export class BaseModel {
+  constructor(
+    private readonly _json: Record<string, any>,
+  ) {}
+
+  json(key?: string | number): any {
+    if (key === undefined) return this._json;
+    if (!this._json) return;
+    return this._json[String(key)];
+  }
+}

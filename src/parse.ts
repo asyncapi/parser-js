24a6eeaef3668850fcfb20879444ce89c34f4cb0
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2022-03-09 18:04:50 +0100

    refactor: init parse, lint and validate functions (#487)

---
+++ src/parse.ts
@@ -0,0 +1,64 @@
+import { AsyncAPIDocument } from "./models";
+import { normalizeInput, toAsyncAPIDocument } from "./utils";
+import { validate } from "./lint";
+
+import type { ParserInput, ParserOutput } from './types';
+import type { ValidateOptions } from './lint';
+
+export interface ParseOptions {
+  applyTraits?: boolean;
+  validateOptions?: ValidateOptions;
+}
+
+export async function parse(asyncapi: ParserInput, options?: ParseOptions): Promise<ParserOutput> {
+  let maybeDocument = toAsyncAPIDocument(asyncapi);
+  if (maybeDocument) {
+    return { 
+      source: asyncapi,
+      parsed: maybeDocument,
+      diagnostics: [],
+    };
+  }
+
+  try {
+    const document = normalizeInput(asyncapi as Exclude<ParserInput, AsyncAPIDocument>);
+    options = normalizeOptions(options);
+
+    const { validated, diagnostics } = await validate(document, options.validateOptions);
+    if (validated === undefined) {
+      return {
+        source: asyncapi,
+        parsed: undefined,
+        diagnostics,
+      };
+    }
+
+    const parsed = new AsyncAPIDocument(validated as Record<string, unknown>);
+    return { 
+      source: asyncapi,
+      parsed,
+      diagnostics,
+    };
+  } catch(err) {
+    // TODO: throw proper error
+    throw Error();
+  }
+}
+
+const defaultOptions: ParseOptions = {
+  applyTraits: true,
+};
+function normalizeOptions(options?: ParseOptions): ParseOptions {
+  if (!options || typeof options !== 'object') {
+    return defaultOptions;
+  }
+  // shall copy
+  options = { ...defaultOptions, ...options };
+
+  // traits
+  if (options.applyTraits === undefined) {
+    options.applyTraits = true;
+  }
+
+  return options;
+}

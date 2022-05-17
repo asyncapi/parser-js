62f5ffe8901a312c8bafd7911d76ec539eda1c34
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2022-05-17 12:07:08 +0200

    refactor: stop with v3 implementation (#551)

--- src/models/asyncapi.ts
+++ src/models/asyncapi.ts
@@ -1,5 +1,4 @@
 import { AsyncAPIDocumentV2 } from "./v2";
-import { AsyncAPIDocumentV3 } from "./v3";
 
 import type { InfoInterface } from "./info";
 import type { BaseModel } from "./base";
@@ -17,8 +16,8 @@ export function newAsyncAPIDocument(asyncapi: DetailedAsyncAPI): AsyncAPIDocumen
   switch (asyncapi.semver.major) {
     case 2:
       return new AsyncAPIDocumentV2(asyncapi.parsed, { asyncapi, pointer: '/' });
-    case 3:
-      return new AsyncAPIDocumentV3(asyncapi.parsed, { asyncapi, pointer: '/' });
+    // case 3:
+    //   return new AsyncAPIDocumentV3(asyncapi.parsed, { asyncapi, pointer: '/' }) as any;
     default:
       throw new Error(`Unsupported AsyncAPI version: ${asyncapi.semver.version}`);
   }

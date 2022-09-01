4e1d57aafbe0dd28e37c4f79a8ec531f70f8aba7
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2022-09-01 10:26:26 +0200

    refactor: add rule to check if AsyncAPI document is defined or not (#583)

--- src/schema-parser/asyncapi-schema-parser.ts
+++ src/schema-parser/asyncapi-schema-parser.ts
@@ -2,6 +2,8 @@ import Ajv from "ajv";
 // @ts-ignore
 import specs from '@asyncapi/specs';
 
+import { specVersions } from '../constants';
+
 import type { ErrorObject, ValidateFunction } from "ajv";
 import type { AsyncAPISchema, SchemaValidateResult } from '../types';
 import type { SchemaParser, ParseSchemaInput, ValidateSchemaInput } from "../schema-parser";
@@ -11,8 +13,6 @@ const ajv = new Ajv({
   strict: false,
   logger: false,
 });
-// Only versions compatible with JSON Schema Draf-07 are supported.
-const specVersions = Object.keys(specs).filter((version: string) => !['1.0.0', '1.1.0', '1.2.0', '2.0.0-rc1', '2.0.0-rc2'].includes(version));
 
 export function AsyncAPISchemaParser(): SchemaParser {
   return {

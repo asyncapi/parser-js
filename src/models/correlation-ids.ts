b5ce5d980baefbbca99e26134c797d97217296eb
Sergio Moya <1083296+smoya@users.noreply.github.com>
2022-09-01 14:04:49 +0200

    refactor: adapt components to parser-api (#586)

---
+++ src/models/correlation-ids.ts
@@ -0,0 +1,4 @@
+import type { Collection } from './collection';
+import type { CorrelationIdInterface } from './correlation-id';
+
+export interface CorrelationIdsInterface extends Collection<CorrelationIdInterface> { }

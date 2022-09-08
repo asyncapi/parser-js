7e99ff286c747c9cc180e083841d5bd70915caa2
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2022-09-08 10:11:18 +0200

    refactor: handle circular references in schemas (#591)

--- test/parse.spec.ts
+++ test/parse.spec.ts
@@ -24,7 +24,7 @@ describe('parse()', function() {
     const document = {
       asyncapi: '2.0.0',
       info: {
-        title: 'Valid AsyncApi document',
+        title: 'Invalid AsyncApi document',
         version: '1.0',
       },
     };
@@ -32,5 +32,124 @@ describe('parse()', function() {
     
     expect(parsed).toEqual(undefined);
     expect(diagnostics.length > 0).toEqual(true);
+  });
+
+  it('should preserve references', async function() {
+    const document = {
+      asyncapi: '2.0.0',
+      info: {
+        title: 'Valid AsyncApi document',
+        version: '1.0',
+      },
+      channels: {
+        channel: {
+          publish: {
+            operationId: 'publishOperation',
+            message: {
+              $ref: '#/components/messages/message'
+            }
+          },
+          subscribe: {
+            operationId: 'subscribeOperation',
+            message: {
+              $ref: '#/components/messages/message'
+            }
+          }
+        }
+      },
+      components: {
+        messages: {
+          message: {
+            payload: {
+              type: 'object',
+            }
+          }
+        }
+      }
+    };
+    const { parsed } = await parse(parser, document);
+    
+    const publishMessage = parsed?.channels().get('channel')?.operations().get('publishOperation')?.messages()[0];
+    const subscribeMessage = parsed?.channels().get('channel')?.operations().get('subscribeOperation')?.messages()[0];
+    const componentsMessage = parsed?.components()?.messages()?.get('message');
+    expect(publishMessage?.json() !== undefined).toEqual(true);
+    expect(subscribeMessage?.json() !== undefined).toEqual(true);
+    expect(componentsMessage?.json() !== undefined).toEqual(true);
+    expect(componentsMessage?.json() === publishMessage?.json()).toEqual(true);
+    expect(componentsMessage?.json() === subscribeMessage?.json()).toEqual(true);
+    expect(publishMessage?.json() === subscribeMessage?.json()).toEqual(true);
+  });
+
+  it('should parse circular references (in this same document)', async function() {
+    const document = {
+      asyncapi: '2.0.0',
+      info: {
+        title: 'Valid AsyncApi document',
+        version: '1.0',
+      },
+      channels: {
+        channel: {
+          publish: {
+            operationId: 'someId',
+            message: {
+              payload: {
+                properties: {
+                  nonCircular: {
+                    type: 'string',
+                  },
+                  circular: {
+                    $ref: '#/channels/channel/publish/message/payload'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+    const { parsed } = await parse(parser, document);
+
+    const messagePayload = parsed?.channels().get('channel')?.operations().get('someId')?.messages()[0].payload();
+    expect(messagePayload?.json() !== undefined).toEqual(true);
+    expect(messagePayload?.properties()?.['circular'].json() !== undefined).toEqual(true);
+    expect(messagePayload?.properties()?.['circular'].json() === messagePayload?.json()).toEqual(true); // expect that same reference
+  });
+
+  it('should parse circular references (in external file)', async function() {
+    const document = {
+      asyncapi: '2.0.0',
+      info: {
+        title: 'Valid AsyncApi document',
+        version: '1.0',
+      },
+      channels: {
+        channel: {
+          publish: {
+            operationId: 'someId',
+            message: {
+              payload: {
+                properties: {
+                  nonCircular: {
+                    type: 'string',
+                  },
+                  circular: {
+                    $ref: './mocks/parse/circular-ref.yaml'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+    const { parsed } = await parse(parser, document, { validateOptions: { path: __filename } });
+
+    const messagePayload = parsed?.channels().get('channel')?.operations().get('someId')?.messages()[0].payload();
+    const circular = messagePayload?.properties()?.['circular'];
+    const deepProperty = circular?.properties()?.['deepProperty'];
+    const deepCircular = deepProperty?.properties()?.['circular'];
+    expect(deepProperty?.json() !== undefined).toEqual(true);
+    expect(deepCircular?.json() !== undefined).toEqual(true);
+    expect(deepProperty?.json() === deepCircular?.json()).toEqual(true); // expect that same reference
   });
 });

9e6da02c6d777cb58044d65a6640246d84c6bf35
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2023-02-09 13:41:14 +0100

    feat: validate unparsed AsyncAPI document (#720)

--- test/ruleset/rules/asyncapi-document-unresolved.spec.ts
+++ test/ruleset/rules/asyncapi-document-unresolved.spec.ts
@@ -1,90 +1,85 @@
-// import { testRule, DiagnosticSeverity } from '../tester';
+import { testRule, DiagnosticSeverity } from '../tester';
 
-describe('asyncapi-document-unresolved', () => {
-  it.todo('add tests after fixing https://github.com/asyncapi/spec-json-schemas/issues/296');
-});
+testRule('asyncapi-document-unresolved', [
+  {
+    name: 'valid case',
+    document: {
+      asyncapi: '2.0.0',
+      info: {
+        title: 'Valid AsyncApi document',
+        version: '1.0',
+      },
+      channels: {
+        someChannel: {
+          publish: {
+            message: {
+              $ref: '#/components/messages/someMessage',
+            },
+          },
+        },
+      },
+      components: {
+        messages: {
+          someMessage: {},
+        },
+      },
+    },
+    errors: [],
+  },
 
-// 
-// testRule('asyncapi-document-unresolved', [
-//   {
-//     name: 'valid case',
-//     document: {
-//       asyncapi: '2.0.0',
-//       info: {
-//         title: 'Valid AsyncApi document',
-//         version: '1.0',
-//       },
-//       channels: {
-//         someChannel: {
-//           publish: {
-//             message: {
-//               $ref: '#/components/messages/someMessage',
-//             },
-//           },
-//         },
-//       },
-//       components: {
-//         messages: {
-//           someMessage: {},
-//         },
-//       },
-//     },
-//     errors: [],
-//   },
+  {
+    name: 'invalid case (reference for operation object is not allowed)',
+    document: {
+      asyncapi: '2.0.0',
+      info: {
+        title: 'Valid AsyncApi document',
+        version: '1.0',
+      },
+      channels: {
+        someChannel: {
+          publish: {
+            $ref: '#/components/x-operations/someOperation',
+          },
+        },
+      },
+      components: {
+        'x-operations': {
+          someOperation: {},
+        },
+      },
+    },
+    errors: [
+      {
+        message: 'Referencing in this place is not allowed',
+        path: ['channels', 'someChannel', 'publish'],
+        severity: DiagnosticSeverity.Error,
+      },
+    ],
+  },
 
-//   {
-//     name: 'invalid case (reference for operation object is not allowed)',
-//     document: {
-//       asyncapi: '2.0.0',
-//       info: {
-//         title: 'Valid AsyncApi document',
-//         version: '1.0',
-//       },
-//       channels: {
-//         someChannel: {
-//           publish: {
-//             $ref: '#/components/x-operations/someOperation',
-//           },
-//         },
-//       },
-//       components: {
-//         'x-operations': {
-//           someOperation: {},
-//         },
-//       },
-//     },
-//     errors: [
-//       {
-//         message: 'Referencing in this place is not allowed',
-//         path: ['channels', 'someChannel', 'publish'],
-//         severity: DiagnosticSeverity.Error,
-//       },
-//     ],
-//   },
-
-//   {
-//     name: 'invalid case (case when other errors should also occur but we filter them out - required info field is omitted)',
-//     document: {
-//       asyncapi: '2.0.0',
-//       channels: {
-//         someChannel: {
-//           publish: {
-//             $ref: '#/components/x-operations/someOperation',
-//           },
-//         },
-//       },
-//       components: {
-//         'x-operations': {
-//           someOperation: {},
-//         },
-//       },
-//     },
-//     errors: [
-//       {
-//         message: 'Referencing in this place is not allowed',
-//         path: ['channels', 'someChannel', 'publish'],
-//         severity: DiagnosticSeverity.Error,
-//       },
-//     ],
-//   },
-// ]);
+  {
+    name: 'invalid case (case when other errors should also occur but we filter them out - required info field is omitted)',
+    document: {
+      asyncapi: '2.0.0',
+      channels: {
+        someChannel: {
+          publish: {
+            $ref: '#/components/x-operations/someOperation',
+          },
+        },
+      },
+      components: {
+        'x-operations': {
+          someOperation: {},
+        },
+      },
+    },
+    errors: [
+      {
+        message: 'Referencing in this place is not allowed',
+        path: ['channels', 'someChannel', 'publish'],
+        severity: DiagnosticSeverity.Error,
+      },
+    ],
+  },
+]);

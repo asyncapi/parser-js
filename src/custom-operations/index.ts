37cdacef83cfdc4998f1b84c235dd34b31119b2c
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2023-04-02 15:36:23 +0200

    feat: new trait behaviour

--- src/custom-operations/index.ts
+++ src/custom-operations/index.ts
@@ -1,4 +1,4 @@
-import { applyTraitsV2 } from './apply-traits';
+import { applyTraitsV2, applyTraitsV3 } from './apply-traits';
 import { checkCircularRefs } from './check-circular-refs';
 import { parseSchemasV2 } from './parse-schema';
 import { anonymousNaming } from './anonymous-naming';
@@ -7,18 +7,17 @@ import type { Parser } from '../parser';
 import type { ParseOptions } from '../parse';
 import type { AsyncAPIDocumentInterface } from '../models';
 import type { DetailedAsyncAPI } from '../types';
-import type { v2 } from '../spec-types';
+import type { v2, v3 } from '../spec-types';
 
 export async function customOperations(parser: Parser, document: AsyncAPIDocumentInterface, detailed: DetailedAsyncAPI, options: ParseOptions): Promise<void> {
   switch (detailed.semver.major) {
   case 2: return operationsV2(parser, document, detailed, options);
-  // case 3: return operationsV3(parser, document, detailed, options);
+  case 3: return operationsV3(parser, document, detailed, options);
   }
 }
 
 async function operationsV2(parser: Parser, document: AsyncAPIDocumentInterface, detailed: DetailedAsyncAPI, options: ParseOptions): Promise<void> {
   checkCircularRefs(document);
-  anonymousNaming(document);
 
   if (options.applyTraits) {
     applyTraitsV2(detailed.parsed as v2.AsyncAPIObject);
@@ -26,5 +25,18 @@ async function operationsV2(parser: Parser, document: AsyncAPIDocumentInterface,
   if (options.parseSchemas) {
     await parseSchemasV2(parser, detailed);
   }
+  anonymousNaming(document);
 }
 
+async function operationsV3(parser: Parser, document: AsyncAPIDocumentInterface, detailed: DetailedAsyncAPI, options: ParseOptions): Promise<void> {
+  checkCircularRefs(document);
+
+  if (options.applyTraits) {
+    applyTraitsV3(detailed.parsed as v3.AsyncAPIObject);
+  }
+  // TODO: Support schema parsing in v3
+  // if (options.parseSchemas) {
+  //   await parseSchemasV2(parser, detailed);
+  // }
+  anonymousNaming(document);
+}
---
+++ test/custom-operations/apply-traits-v3.spec.ts
@@ -0,0 +1,159 @@
+import { AsyncAPIDocumentV3 } from '../../src/models';
+import { Parser } from '../../src/parser';
+
+import type { v3 } from '../../src/spec-types';
+
+describe('custom operations - apply traits v3', function() {
+  const parser = new Parser();
+
+  it('should apply traits to operations', async function() {
+    const documentRaw = {
+      asyncapi: '3.0.0',
+      info: {
+        title: 'Valid AsyncApi document',
+        version: '1.0',
+      },
+      operations: {
+        someOperation1: {
+          traits: [
+            {
+              description: 'some description' 
+            },
+            {
+              description: 'another description' 
+            }
+          ]
+        },
+        someOperation2: {
+          description: 'root description',
+          traits: [
+            {
+              description: 'some description' 
+            },
+            {
+              description: 'another description' 
+            }
+          ]
+        }
+      }
+    };
+    const { document } = await parser.parse(documentRaw);
+    
+    const v3Document = document as AsyncAPIDocumentV3;
+    expect(v3Document).toBeInstanceOf(AsyncAPIDocumentV3);
+
+    const someOperation1 = v3Document?.json()?.operations?.someOperation1;
+    delete someOperation1?.traits;
+    expect(someOperation1).toEqual({ description: 'another description' });
+
+    const someOperation2 = v3Document?.json()?.operations?.someOperation2;
+    delete someOperation2?.traits;
+    expect(someOperation2).toEqual({ description: 'root description' });
+  });
+
+  it('should apply traits to messages (channels)', async function() {
+    const documentRaw = {
+      asyncapi: '3.0.0',
+      info: {
+        title: 'Valid AsyncApi document',
+        version: '1.0',
+      },
+      channels: {
+        someChannel1: {
+          messages: [
+            {
+              traits: [
+                {
+                  messageId: 'traitMessageId',
+                  description: 'some description' 
+                },
+                {
+                  description: 'another description' 
+                }
+              ]
+            }
+          ]
+        },
+        someChannel2: {
+          messages: [
+            {
+              messageId: 'rootMessageId',
+              description: 'root description',
+              traits: [
+                {
+                  messageId: 'traitMessageId',
+                  description: 'some description' 
+                },
+                {
+                  description: 'another description' 
+                }
+              ]
+            }
+          ]
+        }
+      }
+    };
+    const { document } = await parser.parse(documentRaw);
+    
+    const v3Document = document as AsyncAPIDocumentV3;
+    expect(v3Document).toBeInstanceOf(AsyncAPIDocumentV3);
+
+    const message1 = v3Document?.json()?.channels?.someChannel1?.messages?.[0];
+    delete (message1 as v3.MessageObject)?.traits;
+    expect(message1).toEqual({ messageId: 'traitMessageId', description: 'another description', 'x-parser-message-name': 'traitMessageId' });
+
+    const message2 = v3Document?.json()?.channels?.someChannel2?.messages?.[0];
+    delete (message2 as v3.MessageObject)?.traits;
+    expect(message2).toEqual({ messageId: 'rootMessageId', description: 'root description', 'x-parser-message-name': 'rootMessageId' });
+  });
+
+  it('should apply traits to messages (components)', async function() {
+    const documentRaw = {
+      asyncapi: '3.0.0',
+      info: {
+        title: 'Valid AsyncApi document',
+        version: '1.0',
+      },
+      components: {
+        messages: {
+          someMessage1: {
+            traits: [
+              {
+                messageId: 'traitMessageId',
+                description: 'some description' 
+              },
+              {
+                description: 'another description' 
+              }
+            ]
+          },
+          someMessage2: {
+            messageId: 'rootMessageId',
+            description: 'root description',
+            traits: [
+              {
+                messageId: 'traitMessageId',
+                description: 'some description' 
+              },
+              {
+                description: 'another description' 
+              }
+            ]
+          }
+        }
+      }
+    };
+    const { document } = await parser.parse(documentRaw);
+    
+    const v3Document = document as AsyncAPIDocumentV3;
+    expect(v3Document).toBeInstanceOf(AsyncAPIDocumentV3);
+
+    const message1 = v3Document?.json()?.components?.messages?.someMessage1;
+    delete (message1 as v3.MessageObject)?.traits;
+    expect(message1).toEqual({ messageId: 'traitMessageId', description: 'another description', 'x-parser-message-name': 'traitMessageId' });
+
+    const message2 = v3Document?.json()?.components?.messages?.someMessage2;
+    delete (message2 as v3.MessageObject)?.traits;
+    expect(message2).toEqual({ messageId: 'rootMessageId', description: 'root description', 'x-parser-message-name': 'rootMessageId' });
+  });
+});

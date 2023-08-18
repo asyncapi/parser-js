95b6e5e852451625385b5df6223200575771c83b
Sergio Moya <1083296+smoya@users.noreply.github.com>
2023-08-18 14:21:42 +0200

    fix: set proper parser-api version (#849)

--- src/models/asyncapi.ts
+++ src/models/asyncapi.ts
@@ -11,6 +11,9 @@ import type { ServersInterface } from './servers';
 
 import type { v2, v3 } from '../spec-types';
 
+// https://github.com/asyncapi/parser-api/releases/tag/v2.0.0
+export const ParserAPIVersion = '2.0.0';
+
 export interface AsyncAPIDocumentInterface extends BaseModel<v2.AsyncAPIObject | v3.AsyncAPIObject>, ExtensionsMixinInterface {
   version(): string;
   defaultContentType(): string | undefined;

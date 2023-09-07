d42ea8a1532e53fb29d5a23964fd33ede98f5fdd
Sergio Moya <1083296+smoya@users.noreply.github.com>
2023-09-07 17:29:40 +0200

    feat!: optionally include Schema parsers when creating a Parser

--- src/parse.ts
+++ src/parse.ts
@@ -1,36 +1,54 @@
 import { Parser as ParserV2 } from 'parserv2';
 import { Parser as ParserV3 } from 'parserv3';
 
-import type { ParseOptions as ParserOptionsParserV2 } from 'parserv2';
-import type { ParseOptions as ParserOptionsParserV3 } from 'parserv3';
+import { AvroSchemaParser } from '@asyncapi/avro-schema-parser';
+import { OpenAPISchemaParser } from '@asyncapi/openapi-schema-parser';
+import { RamlDTSchemaParser } from '@asyncapi/raml-dt-schema-parser';
+import { ProtoBuffSchemaParser } from '@asyncapi/protobuf-schema-parser';
+
+import type { ParserOptions as ParserOptionsParserV2 } from 'parserv2/esm/parser';
+import type { ParserOptions as ParserOptionsParserV3 } from 'parserv3/esm/parser';
 
 import { majorParserAPIVersion } from './utils';
 
+export type ParserOptions = ParserOptionsParserV2 | ParserOptionsParserV3;
+export type Options = {
+  includeSchemaParsers?: boolean;
+  parserOptions?: ParserOptions;
+}
+
 type Parser = ParserV2 | ParserV3;
 
-// Cache for parsers
-const parsers = new Map<number, Parser>();
+export function NewParser(parserAPIVersion: string, options?: Options): Parser {
+  const parserOptions: ParserOptions = options?.parserOptions || {};
 
-export function NewParser(parserAPIVersion: string, options?: ParserOptionsParserV2 | ParserOptionsParserV3): Parser {
-  const parserAPIMajorVersion = majorParserAPIVersion(parserAPIVersion);
-    
+  // This is done globally instead of per version because latest versions of those schema parsers are still compatible with newer versions of the Parser-JS.
+  // If a breaking change is introduced in the future, then we would need to register the schema parsers compatible with each version of the Parser-JS.
+  if (options?.includeSchemaParsers) {
+    const defaultSchemaParsers = [
+      AvroSchemaParser(),
+      OpenAPISchemaParser(),
+      RamlDTSchemaParser(),
+      ProtoBuffSchemaParser(),
+    ];
+
+    if (!parserOptions.schemaParsers) {
+      parserOptions.schemaParsers = defaultSchemaParsers;
+    } else {
+      // If the user provides a schema parser, use that one instead of the default one. Comparison is done with the mime types.
+      const givenSchemaParsersMimeTypes = parserOptions.schemaParsers.map((schemaParser) => schemaParser.getMimeTypes()).flat();   
+      const filteredDefaultSchemas = defaultSchemaParsers.filter((defaultSchemaParser) => !givenSchemaParsersMimeTypes.includes(defaultSchemaParser.getMimeTypes()[0]));
+      parserOptions.schemaParsers.push(...filteredDefaultSchemas as any);
+    }
+  }
+
+  const parserAPIMajorVersion = majorParserAPIVersion(parserAPIVersion); 
   switch (parserAPIMajorVersion) {
   case 1:
-    if (options) {
-      return new ParserV2(options); // Can't use cached because options is not empty
-    } 
-
-    if (!parsers.has(2)) parsers.set(2, new ParserV2());
-    return parsers.get(2) as ParserV2;
+    return new ParserV2(parserOptions as ParserOptionsParserV2);
   default:
   case 0: // Using Parser v3 (latest atm) by default 
   case 2:
-    if (options) {
-      return new ParserV3(options); // Can't use cached because options is not empty
-    } 
-
-    if (!parsers.has(3)) parsers.set(3, new ParserV3());
-    return parsers.get(3) as ParserV3; 
+    return new ParserV3(parserOptions as ParserOptionsParserV3);
   }   
 }
-
--- test/parse.spec.ts
+++ test/parse.spec.ts
@@ -2,57 +2,91 @@
 import { Parser as ParserV2 } from 'parserv2';
 import { Parser as ParserV3 } from 'parserv3';
 
+import { AvroSchemaParser } from '@asyncapi/avro-schema-parser';
+import { OpenAPISchemaParser } from '@asyncapi/openapi-schema-parser';
+import { RamlDTSchemaParser } from '@asyncapi/raml-dt-schema-parser';
+import { ProtoBuffSchemaParser } from '@asyncapi/protobuf-schema-parser';
+
 import { NewParser } from '../src/index';
+import type { Options } from '../src/parse';
+
+const fakeSchemaParser = {
+  validate: jest.fn().mockReturnValue(Promise.resolve(null)),
+  parse: jest.fn().mockReturnValue(Promise.resolve({})),
+  getMimeTypes: jest.fn().mockReturnValue(['fake-format']),
+};
 
 describe('NewParser()', function() {
   it('Creates a Parser without options compatible with Parser-API v1 and caches it', async function() {
     const parser = NewParser('1.0.0');
     expect(parser).toBeInstanceOf(ParserV2);
-
-    // Test cached parser is the same as the one we previously created.
-    expect(NewParser('1.0.0')).toStrictEqual(parser);
   });
 
-  it('Creates a Parser with options compatible with Parser-API v1 and skips cache', async function() {
-    const options = { applyTraits: false };
+  it('Creates a Parser with options compatible with Parser-API v1', async function() {
+    const options: Options = { parserOptions: { schemaParsers: [fakeSchemaParser]} };
     const parser = NewParser('1.0.0', options);
+    
     expect(parser).toBeInstanceOf(ParserV2);
-
-    // Test no cached parser is returned
-    expect(NewParser('1.0.0', options)).not.toStrictEqual(parser);
+    expect(parser.parserRegistry.get('fake-format')).not.toBeUndefined();
   });
 
-  it('Creates a Parser without options compatible with Parser-API v2 and caches it', async function() {
+  it('Creates a Parser with options including known Schema Parsers and do not overwrite those with Parser-API v1', async function() {
+    const knownSchemaParser = AvroSchemaParser();
+    const options: Options = { parserOptions: { schemaParsers: [knownSchemaParser]}, includeSchemaParsers: true };
+    const parser = NewParser('1.0.0', options);
+    
+    expect(parser).toBeInstanceOf(ParserV2);
+    expect(parser.parserRegistry.get(knownSchemaParser.getMimeTypes()[0])).toStrictEqual(knownSchemaParser);
+    expect(parser.parserRegistry.get(OpenAPISchemaParser().getMimeTypes()[0])).toEqual(OpenAPISchemaParser());
+    expect(parser.parserRegistry.get(RamlDTSchemaParser().getMimeTypes()[0])).toEqual(RamlDTSchemaParser());
+    expect(parser.parserRegistry.get(ProtoBuffSchemaParser().getMimeTypes()[0])).toEqual(ProtoBuffSchemaParser());
+  });
+
+  it('Creates a Parser without options compatible with Parser-API v2', async function() {
     const parser = NewParser('2.0.0');
     expect(parser).toBeInstanceOf(ParserV3);
-
-    // Test cached parser is the same as the one we previously created.
-    expect(NewParser('2.0.0')).toStrictEqual(parser);
   });
 
-  it('Creates a Parser with options compatible with Parser-API v2 and skips cache', async function() {
-    const options = { applyTraits: false };
+  it('Creates a Parser with options compatible with Parser-API v2', async function() {
+    const options: Options = { parserOptions: { schemaParsers: [fakeSchemaParser]} };
     const parser = NewParser('2.0.0', options);
     expect(parser).toBeInstanceOf(ParserV3);
-
-    // Test no cached parser is returned
-    expect(NewParser('2.0.0', options)).not.toStrictEqual(parser);
+    expect(parser.parserRegistry.get('fake-format')).not.toBeUndefined();
   });
 
-  it('Creates a Parser without options compatible with old Parser API (AKA v0) and caches it', async function() {
+  it('Creates a Parser with options including known Schema Parsers and do not overwrite those with Parser-API v2', async function() {
+    const knownSchemaParser = AvroSchemaParser();
+    const options: Options = { parserOptions: { schemaParsers: [knownSchemaParser]}, includeSchemaParsers: true };
+    const parser = NewParser('2.0.0', options);
+    
+    expect(parser).toBeInstanceOf(ParserV3);
+    expect(parser.parserRegistry.get(knownSchemaParser.getMimeTypes()[0])).toStrictEqual(knownSchemaParser);
+    expect(parser.parserRegistry.get(OpenAPISchemaParser().getMimeTypes()[0])).toEqual(OpenAPISchemaParser());
+    expect(parser.parserRegistry.get(RamlDTSchemaParser().getMimeTypes()[0])).toEqual(RamlDTSchemaParser());
+    expect(parser.parserRegistry.get(ProtoBuffSchemaParser().getMimeTypes()[0])).toEqual(ProtoBuffSchemaParser());
+  });
+
+  it('Creates a Parser without options compatible with old Parser API (AKA v0)', async function() {
     const parser = NewParser(''); // could be '0.0.0' as well
     expect(parser).toBeInstanceOf(ParserV3); // Using Parser v3 (latest atm) by default
-
-    // Test cached parser is the same as the one we previously created.
-    expect(NewParser('')).toStrictEqual(parser);
   });
 
-  it('Creates a Parser with options compatible with old Parser API (AKA v0) and skips cache', async function() {
-    const options = { applyTraits: false };
+  it('Creates a Parser with options compatible with old Parser API (AKA v0)', async function() {
+    const options: Options = { parserOptions: { schemaParsers: [fakeSchemaParser]} };
     const parser = NewParser('0.0.0', options); // could be empty string as well
     expect(parser).toBeInstanceOf(ParserV3);
+    expect(parser.parserRegistry.get('fake-format')).not.toBeUndefined();
+  });
 
-    // Test no cached parser is returned
-    expect(NewParser('0.0.0', options)).not.toStrictEqual(parser);
+  it('Creates a Parser with options including known Schema Parsers and do not overwrite those with Parser-API v2', async function() {
+    const knownSchemaParser = AvroSchemaParser();
+    const options: Options = { parserOptions: { schemaParsers: [knownSchemaParser]}, includeSchemaParsers: true };
+    const parser = NewParser('0.0.0', options);
+    
+    expect(parser).toBeInstanceOf(ParserV3);
+    expect(parser.parserRegistry.get(knownSchemaParser.getMimeTypes()[0])).toStrictEqual(knownSchemaParser);
+    expect(parser.parserRegistry.get(OpenAPISchemaParser().getMimeTypes()[0])).toEqual(OpenAPISchemaParser());
+    expect(parser.parserRegistry.get(RamlDTSchemaParser().getMimeTypes()[0])).toEqual(RamlDTSchemaParser());
+    expect(parser.parserRegistry.get(ProtoBuffSchemaParser().getMimeTypes()[0])).toEqual(ProtoBuffSchemaParser());
   });
 });

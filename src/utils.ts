ae043a8d40e148cd35394f0e67ebd3a92680944b
Sergio Moya <1083296+smoya@users.noreply.github.com>
2023-10-03 17:25:57 +0200

    feat!: change Parser-API version args to represent just the major

--- src/utils.ts
+++
@@ -1,5 +0,0 @@
-export function majorParserAPIVersion(version: string): number {
-  if (!version) return 0;
-    
-  return Number(version.split('.')[0]);
-}
--- test/convert.spec.ts
+++ test/convert.spec.ts
@@ -9,11 +9,12 @@ describe('ConvertDocumentParserAPIVersion()', function() {
     const doc = { asyncapi: '2.6.0', info: { title: '', description: '', version: ''}, channels: {} };
     const parsedDocParserV2 = (await new ParserV2().parse(doc)).document as AsyncAPIDocument;
 
-    // Even though the value here is '1' for Parser V3, we force to be '2.0.0' for this test to do the equality check later.
-    parsedDocParserV2['_json']['x-parser-api-version'] = '2.0.0';
+    // Even though the value here is 1 for Parser V3, we force to be 2 for this test to do the equality check later.
+    parsedDocParserV2['_json']['x-parser-api-version'] = 2;
 
     const parsedDocParserV3 = await new ParserV3().parse(doc);
-    const convertedDoc = ConvertDocumentParserAPIVersion(parsedDocParserV2, '2.0.0');
+    const convertedDoc = ConvertDocumentParserAPIVersion(parsedDocParserV2, 2);
+
     expect(convertedDoc).toEqual(parsedDocParserV3.document);
   });
 
@@ -21,17 +22,17 @@ describe('ConvertDocumentParserAPIVersion()', function() {
     const doc = { asyncapi: '2.6.0', info: { title: '', description: '', version: ''}, channels: {} };
     const parsedDocParserV3 = (await new ParserV3().parse(doc)).document as AsyncAPIDocument;
 
-    // Even though the value here is '2.0.0' for Parser V3, we force to be 1 for this test to do the equality check later.
+    // Even though the value here is 2 for Parser V3, we force to be 1 for this test to do the equality check later.
     parsedDocParserV3['_json']['x-parser-api-version'] = 1;
 
     const parsedDocParserV2 = (await new ParserV2().parse(doc)).document;
-    const convertedDoc = ConvertDocumentParserAPIVersion(parsedDocParserV3, '1.0.0');
+    const convertedDoc = ConvertDocumentParserAPIVersion(parsedDocParserV3, 1);
     expect(convertedDoc).toEqual(parsedDocParserV2);
   });
   
   it('Skips converting if no document is passed', async function() {
     const doc = { } as AsyncAPIDocument;
-    const convertedDoc = ConvertDocumentParserAPIVersion(doc, '1.0.0');
+    const convertedDoc = ConvertDocumentParserAPIVersion(doc, 1);
     expect(convertedDoc).toEqual(doc);
   });
 });

2e7953ce8e85d0640e7225608b315c769b628409
Sergio Moya <1083296+smoya@users.noreply.github.com>
2023-11-27 12:56:44 +0100

    improve test by testing multiple messages in operation

--- test/ruleset/rules/v3/asyncapi3-operation-messages-from-referred-channel.spec.ts
+++ test/ruleset/rules/v3/asyncapi3-operation-messages-from-referred-channel.spec.ts
@@ -153,8 +153,7 @@ testRule('asyncapi3-operation-messages-from-referred-channel', [
     },
     errors: [
       {
-        message:
-          'Operation message does not belong to the specified channel.',
+        message: 'Operation message does not belong to the specified channel.',
         path: ['operations', 'UserSignedUp', 'messages', '0'],
         severity: DiagnosticSeverity.Error,
       }
@@ -224,9 +223,99 @@ testRule('asyncapi3-operation-messages-from-referred-channel', [
     },
     errors: [
       {
-        message:
-          'Operation message does not belong to the specified channel.',
+        message: 'Operation message does not belong to the specified channel.',
         path: ['components', 'operations', 'UserSignedUp', 'messages', '0'],
+        severity: DiagnosticSeverity.Error,
+      }
+    ],
+  },
+  {
+    name: 'invalid case - multiple messages from operation in components pointing to multiple message from a different channel defined under components',
+    document: {
+      asyncapi: '3.0.0',
+      info: {
+        title: 'Account Service',
+        version: '1.0.0'
+      },
+      channels: {
+        UserSignedUp: {
+          messages: {
+            UserSignedUp: {
+              payload: {
+                type: 'object',
+                properties: {
+                  displayName: {
+                    type: 'string'
+                  },
+                  email: {
+                    type: 'string'
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+      components: {
+        channels: {
+          UserRemoved: {
+            messages: {
+              UserRemoved: {
+                payload: {
+                  type: 'object',
+                  properties: {
+                    displayName: {
+                      type: 'string'
+                    },
+                    email: {
+                      type: 'string'
+                    }
+                  }
+                }
+              },
+              UserDeleted: {
+                payload: {
+                  type: 'object',
+                  properties: {
+                    displayName: {
+                      type: 'string'
+                    },
+                    email: {
+                      type: 'string'
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        operations: {
+          UserSignedUp: {
+            action: 'send',
+            channel: {
+              $ref: '#/channels/UserSignedUp'
+            },
+            messages: [
+              {
+                $ref: '#/components/channels/UserRemoved/messages/UserRemoved'
+              },
+              {
+                $ref: '#/components/channels/UserRemoved/messages/UserDeleted'
+              }
+            ]
+          }
+        },
+      }
+    },
+    errors: [
+      {
+        message: 'Operation message does not belong to the specified channel.',
+        path: ['components', 'operations', 'UserSignedUp', 'messages', '0'],
+        severity: DiagnosticSeverity.Error,
+      },
+      {
+        message: 'Operation message does not belong to the specified channel.',
+        path: ['components', 'operations', 'UserSignedUp', 'messages', '1'],
         severity: DiagnosticSeverity.Error,
       }
     ],

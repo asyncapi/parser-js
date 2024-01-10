165ef88d25f08bc9c4e442ae0560520d1b094058
Jonas Lagoni <jonas-lt@live.dk>
2024-01-09 16:00:24 -1000

    update config

---
+++ webpack_new.config.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  target: 'web',
+  mode: 'production',
+
+  output: {
+    path: path.resolve(__dirname, 'browser_new'),
+    filename: 'index.js',
+    globalObject: '(typeof self !== \'undefined\' ? self : this)',
+    library: {
+      name: 'AsyncAPIParser',
+      type: 'umd'
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          configFile: 'tsconfig.json',
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    fallback: {
+      fs: false,
+      path: false,
+      util: false,
+      buffer: require.resolve('buffer/'),
+    }
+  },
+
+  plugins: [
+    /**
+     * Uncomment plugin when you wanna see dependency map of bundled package
+     */
+    // (require('webpack-bundle-analyzer').BundleAnalyzerPlugin()),
+  ],
+};

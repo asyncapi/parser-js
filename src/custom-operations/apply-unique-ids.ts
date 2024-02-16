eea6ac99ca5dc2d12408a3e2f3fc22d0861be2ea
Jonas Lagoni <jonas-lt@live.dk>
2024-02-16 12:12:08 +0100

    fix traits

--- src/custom-operations/apply-unique-ids.ts
+++ src/custom-operations/apply-unique-ids.ts
@@ -3,23 +3,42 @@ import { xParserObjectUniqueId } from '../constants';
 /**
  * This function applies unique ids for objects whose key's function as ids, ensuring that the key is part of the value.
  * 
- * For v3; Apply unique ids to channel's, and message's
+ * For v3; Apply unique ids to all channel's, operations, and message's.
+ * 
+ * Does not apply unique ids to $ref objects
  */
 export function applyUniqueIds(structure: any) {
   const asyncapiVersion = structure.asyncapi.charAt(0);
   switch (asyncapiVersion) {
   case '3':
-    if (structure.channels) {
-      for (const [channelId, channel] of Object.entries(structure.channels as Record<string, any>)) {
-        channel[xParserObjectUniqueId] = channelId;
-        if (channel.messages) {
-          for (const [messageId, message] of Object.entries(channel.messages as Record<string, any>)) {
-            message[xParserObjectUniqueId] = messageId; 
-          }
-        }
-      }
+    applyUniqueIdToChannels(structure.channels);
+    applyUniqueIdToObjects(structure.operations);
+    if (structure.components) {
+      applyUniqueIdToObjects(structure.components.messages);
+      applyUniqueIdToObjects(structure.components.operations);
+      applyUniqueIdToChannels(structure.components.channels);
     }
     break;
   }
 }
-  
+
+function applyUniqueIdToChannels(channels: any) {
+  for (const [channelId, channel] of Object.entries((channels ?? {}) as Record<string, any>)) {
+    if (!channel.$ref) {
+      channel[xParserObjectUniqueId] = channelId;
+    }
+    for (const [messageId, message] of Object.entries((channel.messages ?? {}) as Record<string, any>)) {
+      if (!message.$ref) {
+        message[xParserObjectUniqueId] = messageId;
+      }
+    }
+  }
+}
+
+function applyUniqueIdToObjects(objects: any) {
+  for (const [objectKey, object] of Object.entries((objects ?? {}) as Record<string, any>)) {
+    if (!object.$ref) {
+      object[xParserObjectUniqueId] = objectKey;
+    }
+  }
+}

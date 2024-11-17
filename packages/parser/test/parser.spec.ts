8f686f53f37f129213d6e7bbfee02edffc4a50de
Daniel Ramos <danielramosacosta1@gmail.com>
2024-11-17 12:46:00 +0100

    add test reproducing the error

--- packages/parser/test/parser.spec.ts
+++ packages/parser/test/parser.spec.ts
@@ -1,6 +1,9 @@
+import path from 'path';
+import fs from 'fs';
 import { Spectral } from '@stoplight/spectral-core';
 import { Parser } from '../src/parser';
 import { AsyncAPISchemaParser } from '../src/schema-parser/asyncapi-schema-parser';
+import { AsyncAPIDocumentInterface } from "../src";
 
 describe('Parser class', function() {
   it('should create Parser instance', async function() {
@@ -17,5 +20,21 @@ describe('Parser class', function() {
     const parser = new Parser();
     parser.registerSchemaParser(AsyncAPISchemaParser());
     expect((parser as any).parserRegistry.size).toBeGreaterThan(1);
+  });
+
+  it('should give correct names to external references', async function() {
+    const parser = new Parser();
+    const source = path.resolve(__dirname, './mocks/simple-with-external-refs.yaml');
+    const nestedSchemas = fs.readFileSync(source, 'utf8');
+
+    const { document } = await parser.parse(nestedSchemas, { source });
+
+    const channels = (document as AsyncAPIDocumentInterface).channels().all();
+    const onUserRegisteredChannel = channels[0]
+    const onUserLoggedChannel = channels[1]
+    const userRegisteredMessagePayload = onUserRegisteredChannel.messages().all()[0].payload()?.json() as any;
+    const userLoggedMessagePayload = onUserLoggedChannel.messages().all()[0].payload()?.json() as any;
+    expect(userRegisteredMessagePayload?.["x-parser-schema-id"]).toBe("UserRegistered");
+    expect(userLoggedMessagePayload?.["x-parser-schema-id"]).toBe("UserLogged");
   });
 });

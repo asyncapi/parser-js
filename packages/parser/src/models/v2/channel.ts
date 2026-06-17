9cfb7e97cc83d555fd38b95d27d6449e059a207a
AayushSaini101 <kumaraayush9810@gmail.com>
2026-06-17 07:04:23 +0530

    Remove channel infor from the v2 version

--- packages/parser/src/models/v2/channel.ts
+++ packages/parser/src/models/v2/channel.ts
@@ -39,14 +39,6 @@ export class Channel extends BaseModel<v2.ChannelObject, { id: string, address: 
     return description(this);
   }
 
-  hasTitle(): boolean {
-    return false;
-  }
-
-  title(): string | undefined {
-    return undefined;
-  }
-
   servers(): ServersInterface {
     const servers: ServerInterface[] = [];
     const allowedServers: string[] = this._json.servers || [];
